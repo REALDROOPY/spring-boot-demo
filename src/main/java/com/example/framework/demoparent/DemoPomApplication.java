994ab2065e7436e304913057ceb570a47d31220c
jiangkun <jiangkun@chexiang.com>
2018-08-10 16:32:36 +0800

    init

---
+++ src/main/java/com/example/framework/demoparent/DemoPomApplication.java
@@ -0,0 +1,12 @@
+package com.example.framework.demoparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPomApplication.class, args);
+	}
+}
